d66ee70d2383ad0b11c345879e30ff1207a684e1
Robin Malhotra <me@rmalhotra.com>
2019-05-13 18:29:27 +0530

    target _blanked the links from remark

--- src/components/MarkdowneyBullet.js
+++ src/components/MarkdowneyBullet.js
@@ -1,11 +1,13 @@
 import React from "react"
 import remark from 'remark'
 import remark2react from 'remark-react'
+import externalLinks from 'remark-external-links'
 
 class MarkdowneyBullet extends React.Component {
   render() {
     return (
       remark()
+        .use(externalLinks, { target: "_blank" })
         .use(remark2react)
         .processSync(this.props.text).contents
     )
